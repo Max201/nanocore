20f8037e0cff7d1347abd51b48fc75d6b84fc500
stassyn <alexskripkoit@gmail.com>
2016-01-05 09:20:27 +0200

    Fixed DB Connection charset & login form style

--- engine/ActiveRecord/lib/Connection.php
+++ engine/ActiveRecord/lib/Connection.php
@@ -200,8 +200,12 @@ abstract class Connection
 
 			$this->connection = new PDO("$info->protocol:$host;dbname=$info->db",$info->user,$info->pass,static::$PDO_OPTIONS);
 
-            @$this->connection->query("SET NAMES 'utf8';");
-            @$this->connection->query("SET CHARACTER SET utf8;");
+            if($info->protocol=='mysql') {
+                @$this->connection->query("SET NAMES 'utf8'");
+                @$this->connection->query("SET CHARACTER SET utf8");
+                @$this->connection->query("SET character_set_connection=utf8");
+                @$this->connection->query("SET character_set_client=utf8");
+            }
 		} catch (PDOException $e) {
 			throw new DatabaseException($e);
 		}
