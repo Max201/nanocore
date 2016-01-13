546f1bf3c5cd7d3a8c26c44b677081286af2b44d
NanoCore <cods.max@gmail.com>
2016-01-13 03:19:41 +0200

    Design & Post module, sitemap

--- engine/Module/Post/Module.php
+++ engine/Module/Post/Module.php
@@ -7,12 +7,15 @@
 namespace Module\Post;
 
 
+use Service\Application\Translate;
 use Service\Module\RecursiveTree;
 use Service\Paginator\Listing;
+use Service\Render\Theme;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use System\Engine\NCModule;
 use System\Engine\NCService;
+use System\Engine\NCSitemapBuilder;
 use System\Environment\Env;
 use System\Environment\Options;
 use System\Util\Calendar;
@@ -25,11 +28,78 @@ use User;
  */
 class Module extends NCModule
 {
+    const SITEMAP = true;
+
     public function route()
     {
         $this->map->addRoute('/', [$this, 'news'], 'news');
         $this->map->addPattern('<id:\d+>-<slug:.+>.html', [$this, 'post'], 'post');
         $this->map->addPattern('category/<id:\d+>', [$this, 'category'], 'category');
+    }
+
+    /**
+     * @param NCSitemapBuilder $builder
+     * @return NCSitemapBuilder
+     */
+    public function sitemap(NCSitemapBuilder $builder)
+    {
+        // News page
+        $builder->add_url(
+            $this->map->reverse('news'),
+            1,
+            'now',
+            'daily'
+        );
+
+        // Posts map
+        $posts = \Post::all();
+        foreach ($posts as $entry) {
+            $builder->add_url(
+                $this->map->reverse('post', [$entry->id, $entry->slug]),
+                0.9,
+                $entry->updated_at,
+                'weekly'
+            );
+        }
+
+        // Categories
+        $categories = \PostCategory::all();
+        foreach ($categories as $category) {
+            $builder->add_url(
+                $this->map->reverse('category', [$category->id]),
+                0.9,
+                mktime(0, 0, 0),
+                'daily'
+            );
+        }
+
+        return $builder;
+    }
+
+    /**
+     * @param NCModule $module
+     * @param Theme $view
+     * @param Translate $lang
+     * @return array
+     */
+    public static function globalize(NCModule $module, Theme $view, Translate $lang)
+    {
+        $categories = \PostCategory::listing();
+        /** @var RecursiveTree $recursive_tree */
+        $recursive_tree = NCService::load('Module.RecursiveTree', [$categories]);
+        $categories = array_map(function($item) use($recursive_tree) {
+            $result = $item->to_array();
+            $result['link'] = '/post/category/' . $item->id;
+            $result['posts'] = \Post::count([
+                'conditions'    => ['category_id IN (?)', $recursive_tree->childs($item->id)]
+            ]);
+
+            return $result;
+        }, $categories);
+
+        return [
+            'post_categories'   => $categories
+        ];
     }
 
     /**
@@ -63,10 +133,12 @@ class Module extends NCModule
             // Ordering
             if ( $request->order ) {
                 $filter['order'] = $request->order;
+            } else {
+                $filter['order'] = 'updated_at DESC';
             }
 
             // Get posts
-            $posts = \Post::find($filter);
+            $posts = \Post::all($filter);
 
             // Rendering
             return $this->view->render('posts/list.twig', [
@@ -131,7 +203,7 @@ class Module extends NCModule
         }
 
         // Get posts
-        $posts = \Post::find($filter);
+        $posts = \Post::all($filter);
 
         // Rendering
         return $this->view->render('posts/list.twig', [
